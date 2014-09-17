2437696f2112711fd144f98be2def6706565b174
Michael Ficarra <git@michael.ficarra.me>
2014-09-17 10:58:59 -0500

    add scope support
    warning: context.getScope differs from eslint in that it is
    parameterised by an AST node

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var EventEmitter = require('events').EventEmitter;
 
+var escope = require('escope');
 var esprima = require('esprima');
 var Dispatcher = require('esdispatch');
 var TokenManager = require('./lib/TokenManager');
@@ -52,6 +53,22 @@ function reporterGeneratorGenerator(emitter) {
   }
 }
 
+function getScopeGenerator(scopes) {
+  var scopeLookupTable = {};
+  for (var i = 0, l = scopes.length; i < l; ++i) {
+    var scope = scopes[i];
+    if (!scope.functionExpressionScope) {
+      scopeLookupTable[scope.block.range[0]] = scope;
+    }
+  }
+  return function getScope(node) {
+    var index = node.range[0];
+    if (index != null && {}.hasOwnProperty.call(scopeLookupTable, index)) {
+      return scopeLookupTable[index];
+    }
+  };
+}
+
 function Linter(){}
 Linter.prototype = Object.create(EventEmitter.prototype);
 Linter.prototype.lint = function lint(js) {
@@ -80,11 +97,14 @@ Linter.prototype.lint = function lint(js) {
     return;
   }
 
+  var scopes = escope.analyze(ast).scopes;
+
   var dispatcher = new Dispatcher;
   var reporterGenerator = reporterGeneratorGenerator(this);
   var tokenManager = new TokenManager(ast.tokens);
 
   var api = Object.create(tokenManager);
+  api.getScope = getScopeGenerator(scopes);
 
   for (var ruleId in RULES) {
     if (!{}.hasOwnProperty.call(RULES, ruleId)) continue;
